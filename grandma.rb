f3598eb7fb4e8362edb6df2b4011c12f5d11bf00
Your Name <you@example.com>
2018-06-11 11:24:29 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,31 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+  # define the method
+  if phrase == "I LOVE YOU GRANDMA!"
+    #if the phrase equals ILOVEYOU GRADMA it wil return the code below
+    return  "I LOVE YOU TOO PUMPKIN!"
+
+ elsif phrase == phrase.upcase
+#  if phrase equals the phrase Capitalized (.upcase) elsif is another conditonal if the first condition wasn met
+    return  "NO, NOT SINCE 1938!"
+    # it will return code aboce
+  else
+    # if none of the condtional are met it will return the code below
+  return "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+
+
+
+
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
